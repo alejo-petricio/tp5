0ecd68febed05d27d079f55110b0ffb08cf2768f
alejo <alejoepetricio@gmail.com>
2024-06-20 15:23:39 -0300

    tp5 listo

--- calculadora.sh
+++ calculadora.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+while true; do
+
+echo "Seleccione la operación que desea realizar:"
+echo "1. Suma"
+echo "2. Resta"
+echo "3. Multiplicación"
+echo "4. División"
+echo "5. Salir"
+read operacion
+
+if [ $operacion -eq 5 ]
+  then
+   break
+fi
 
 echo "Ingrese el primer número:"
 read num1
@@ -6,37 +20,42 @@ read num1
 echo "Ingrese el segundo número:"
 read num2
 
-echo "Calculadora"
-echo "Seleccione la operación que desea realizar:"
-echo "1. Suma"
-echo "2. Resta"
-echo "3. Multiplicación"
-echo "4. División"
-read operacion
-
 # Realizar la operación
-if [ "$operacion" -eq 1 ]
+if [ $operacion -eq 1 ]
  then
     resultado=$((num1 + num2))
+    echo "-------------------------------"
     echo "El resultado de la suma es: $resultado"
-elif [ "$operacion" -eq 2 ]
- then
+    echo "-------------------------------"
+
+ elif [ $operacion -eq 2 ]
+  then
     resultado=$((num1 - num2))
+    echo "-------------------------------"
     echo "El resultado de la resta es: $resultado"
-elif [ "$operacion" -eq 3 ]
- then
+    echo "-------------------------------"
+
+ elif [ $operacion -eq 3 ]
+  then
     resultado=$((num1 * num2))
+    echo "------------------------------------"
     echo "El resultado de la multiplicación es: $resultado"
-elif [ "$operacion" -eq 4 ]
- then
-    if [ "$num2" -eq 0 ]
+    echo "------------------------------------"
+
+ elif [ $operacion -eq 4 ]
+  then
+    if [ $num2 -eq 0 ]
      then
-        echo "Error: División por cero no permitida."
-    else
+        echo "No se puede dividir por cero"
+     else
         resultado=$((num1 / num2))
+        echo "-----------------------------------"
         echo "El resultado de la división es: $resultado"
+        echo "-----------------------------------"
     fi
+
 else
     echo "Operación no válida."
 fi
 
+done
